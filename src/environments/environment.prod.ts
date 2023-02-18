e0126de171d61f6a2f5e71dbf1efe9609c6603a0
Jaardaa <78655691+Jaardaa@users.noreply.github.com>
2023-02-18 12:48:12 +0100

    Firebase added over AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'rxjsinreal',
+    appId: '1:550519084420:web:1b99a5e27154b552f639d1',
+    storageBucket: 'rxjsinreal.appspot.com',
+    apiKey: 'AIzaSyB-FXwFeK35DRedCXQGN5pPJ274j5IJRww',
+    authDomain: 'rxjsinreal.firebaseapp.com',
+    messagingSenderId: '550519084420',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'rxjsinreal',
+    appId: '1:550519084420:web:1b99a5e27154b552f639d1',
+    storageBucket: 'rxjsinreal.appspot.com',
+    apiKey: 'AIzaSyB-FXwFeK35DRedCXQGN5pPJ274j5IJRww',
+    authDomain: 'rxjsinreal.firebaseapp.com',
+    messagingSenderId: '550519084420',
+  },
+  
   production: false
 };
 
